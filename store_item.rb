5ec970565b10a98be75bf53009cf7607de8862c6
Lucas <lucas.t.ho.lh@gmail.com>
2021-10-06 12:40:50 -0500

    adds attributes to code

--- store_item.rb
+++ store_item.rb
@@ -4,7 +4,8 @@ item3= [{name: "mozzarella", quantity: 12, price: 7.99}]
 
 class Menu
   attr_reader :name, :quantity, :price
-  attr_writer 
+  attr_writer :name, :quantity, :price
+
   def initialize(name, quantity, price)
    @name = name
    @quantity = quantity
@@ -19,6 +20,7 @@ end
 item_1 = Menu.new("fried chicken", 6, 7.99)
 item_2 = Menu.new("dumplings", 8, 6.99)
 item_3 = Menu.new("mozzarella sticks", 12, 7.99)
+item_2.name = "fried dumplings"
 item_1.print_info
 item_2.print_info
 item_3.print_info
