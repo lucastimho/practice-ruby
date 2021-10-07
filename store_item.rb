6c8617a9778b4bf8709bbf9c73edf5cb23027b38
Lucas <lucas.t.ho.lh@gmail.com>
2021-10-07 13:03:24 -0500

    fixes errors with no end of inupt for module

--- store_item.rb
+++ store_item.rb
@@ -2,6 +2,7 @@ module Ratings
   def signature_dish
     puts "Our signature dish includes"
     puts @name
+  end
 end
 
 class Menu
@@ -26,14 +27,4 @@ class Food < Menu
     super
     @shelf_life = text[:shelf_life]
   end
-
-item_1 = Menu.new(name: "fried chicken", quantitiy: 6, price: 7.99)
-item_2 = Menu.new(name: "dumplings", quantity: 8, price: 6.99)
-item_3 = Menu.new(name: "mozzarella sticks", quantity: 12, price: 7.99)
-item_2.name = "fried dumplings"
-item_1.print_info
-item_2.print_info
-item_3.print_info
-item_1.signature_dish
-item_2.signature_dish
-item_3.signature_dish
+end
